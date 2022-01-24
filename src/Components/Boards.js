0f7ccccf818746e88647f2a2e0f538a13c1cd481
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-01-24 10:00:11 +0530

    Snake LinkedList Representation done

--- src/Components/Boards.js
+++ src/Components/Boards.js
@@ -1,25 +1,62 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import "../Styles/Boards.css";
+
+const snakeStartPosition=(board)=>{
+    const startingRow=Math.round(board.length/3);
+    const startingCol=Math.round(board[0].length/3);
+    const startingCell=board[startingRow][startingCol];
+    const val={
+          row:startingRow,
+          col:startingCol,
+          cell:startingCell,
+    };
+    return val;
+}
+
+class LinkedListNode{
+    constructor(value){
+        this.value=value;
+        this.next=null;
+    }
+}
+class SinglyLinkedList{
+  constructor(value){
+    const node = new LinkedListNode(value);
+    this.head = node;
+    this.tail = node;
+  }
+}
+const BOARD_SIZE = 10;
+
+const createBoard = (boardSize) => {
+    const board=[];
+    let cnt=1;
+    for(let i=0; i<boardSize; i++){
+      const row=[];
+      for(let j=0;j<boardSize; j++){
+          row.push(cnt++);
+      }
+      board.push(row);
+    }
+    return board;
+}
 
 const Boards = () => {
 
-  const BOARD_SIZE = 10;
-  //const [board, setBoard] = React.useState(new Array(BOARD_SIZE).fill(0).map(()=>new Array(BOARD_SIZE).fill(0)));
-
-  const createBoard = (boardSize) => {
-      const board=[];
-      let cnt=1;
-      for(let i=0; i<boardSize; i++){
-        const row=[];
-        for(let j=0;j<boardSize; j++){
-            row.push(cnt++);
-        }
-        board.push(row);
-      }
-      return board;
+  
+  const handleKeyDown = (e) => {
+      console.log("Do something");
   }
+  useEffect(()=>{
+    window.addEventListener('keydown', e=>{handleKeyDown(e)});
+  },[])
+
   const [board, setBoard] =useState(createBoard(BOARD_SIZE));
-  const [snakeCell,setSnakeCell] = useState(new Set([45]));
+ 
+ const [snake,setSnake]=useState(new SinglyLinkedList(snakeStartPosition(board)));
+
+  const [snakeCell,setSnakeCell] = useState(new Set([snake.head.value.cell]));
+  
   return (
   <div className="board">
     {board.map((row, rowIndex) =>{
