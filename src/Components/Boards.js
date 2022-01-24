a1425480fde6a5742a6f519e6280a4c82571aae6
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-01-24 10:56:24 +0530

    getting food done

--- src/Components/Boards.js
+++ src/Components/Boards.js
@@ -1,17 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import "../Styles/Boards.css";
 
-const snakeStartPosition=(board)=>{
-    const startingRow=Math.round(board.length/3);
-    const startingCol=Math.round(board[0].length/3);
-    const startingCell=board[startingRow][startingCol];
-    const val={
-          row:startingRow,
-          col:startingCol,
-          cell:startingCell,
-    };
-    return val;
-}
+
 
 class LinkedListNode{
     constructor(value){
@@ -26,7 +16,68 @@ class SinglyLinkedList{
     this.tail = node;
   }
 }
-const BOARD_SIZE = 10;
+const BOARD_SIZE = 12;
+
+
+
+const Boards = () => {
+
+  
+  const handleKeyDown = (e) => {
+      console.log("Do something");
+  }
+  useEffect(()=>{
+    window.addEventListener('keydown', e=>{handleKeyDown(e)});
+  },[])
+
+  const [board, setBoard] =useState(createBoard(BOARD_SIZE));
+  const [snake,setSnake]=useState(new SinglyLinkedList(snakeStartPosition(board)));
+  const [snakeCell,setSnakeCell] = useState(new Set([snake.head.value.cell]));
+  const [foodCell,setFoodCell]=useState(getFoodPosition(snakeCell));
+  const [direction,setDirection]=useState('right');
+  
+  return (
+  <div className="board">
+    {board.map((row, rowIndex) =>{
+        return (
+        <div className="row" key={rowIndex}>
+            {row.map((cellValue) =>{
+                const classNameVal=getClassName(cellValue,foodCell,snakeCell);
+                return (
+                <div className={classNameVal} key={cellValue}>
+                    {cellValue}
+                </div>
+                )
+            })}
+        </div>);
+    })}
+  </div>
+  );
+};
+
+const getClassName = (cellValue,foodCell,snakeCell)=>{
+    let className='cell'; 
+    if(cellValue===foodCell){
+         className='cell food-cell';
+    }
+    if(snakeCell.has(cellValue)){
+        className='cell snake-cell';
+    }
+    return className;
+}
+export default Boards;
+
+const snakeStartPosition=(board)=>{
+    const startingRow=Math.round(board.length/3);
+    const startingCol=Math.round(board[0].length/3);
+    const startingCell=board[startingRow][startingCol];
+    const val={
+          row:startingRow,
+          col:startingCol,
+          cell:startingCell,
+    };
+    return val;
+}
 
 const createBoard = (boardSize) => {
     const board=[];
@@ -41,38 +92,10 @@ const createBoard = (boardSize) => {
     return board;
 }
 
-const Boards = () => {
-
-  
-  const handleKeyDown = (e) => {
-      console.log("Do something");
-  }
-  useEffect(()=>{
-    window.addEventListener('keydown', e=>{handleKeyDown(e)});
-  },[])
-
-  const [board, setBoard] =useState(createBoard(BOARD_SIZE));
- 
- const [snake,setSnake]=useState(new SinglyLinkedList(snakeStartPosition(board)));
-
-  const [snakeCell,setSnakeCell] = useState(new Set([snake.head.value.cell]));
-  
-  return (
-  <div className="board">
-    {board.map((row, rowIndex) =>{
-        return (
-        <div className="row" key={rowIndex}>
-            {row.map((cellValue) =>{
-                return (
-                <div className={`cell ${(snakeCell.has(cellValue))?'snake-cell':''}`} key={cellValue}>
-                    {cellValue}
-                </div>
-                )
-            })}
-        </div>);
-    })}
-  </div>
-  );
-};
-
-export default Boards;
+const getFoodPosition=(snakeCell)=>{
+    let foodPosition=Math.floor(Math.random()*BOARD_SIZE*BOARD_SIZE);
+    while(snakeCell.has(foodPosition)){
+        foodPosition=Math.floor(Math.random()*BOARD_SIZE*BOARD_SIZE);
+    }
+    return foodPosition;
+}
